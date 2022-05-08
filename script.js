8e12289ce9a2c3f90d0ffd870364c50f34ffd8d0
alezxandro <alezxandro.gheogliau@gmail.com>
2022-05-08 02:04:59 +0200

    aggiunta stili e cambio tabella contenuti

--- script.js
+++ script.js
@@ -7,10 +7,12 @@ titles = Array.from(titles);
 titles.forEach (element => {
     let a = document.createElement("a");
     let li = document.createElement("li");
-    li.classList.add("navbar-item");
+    // li.classList.add("navbar-item");
+    // li.classList.add("pd-2rem-x");
+    a.style.fontWeight = "550";
 
     a.innerHTML = element.innerHTML;
-    a.href = element.id;
+    a.href = "#" + element.id;
     li.appendChild(a);
     titlesToc.appendChild(li);
 })
